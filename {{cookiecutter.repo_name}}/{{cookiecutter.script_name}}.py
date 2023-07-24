ef31264bdfc70890f0f4c2386d6e54704cf9e961
Oliver Sauder <os@esite.ch>
2023-07-24 18:50:44 +0400

    Added basic logging

--- {{cookiecutter.repo_name}}/{{cookiecutter.script_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.script_name}}.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import logging
+import sys
 from argparse import ArgumentParser
 
 
@@ -6,13 +8,32 @@ def init_argparse():
     parser = ArgumentParser(
         description="{{cookiecutter.description}}",
     )
+    parser.add_argument(
+        "--log-level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="INFO",
+        help="Set the log level (default: INFO)",
+    )
 
     return parser
 
 
+def configure_logging(log_level):
+    # Initialize logging to standard output (console)
+    logging.basicConfig(
+        stream=sys.stdout,
+        filemode="w",
+        level=getattr(logging, log_level),
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+
 def main():
     parser = init_argparse()
-    parser.parse_args()
+    args = parser.parse_args()
+
+    configure_logging(args.log_level)
 
 
 if __name__ == "__main__":
